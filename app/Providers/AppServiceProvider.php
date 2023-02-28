b3529344ee0132316a2d80288854955e3ed33329
Wagura Maurice <business@waguramaurice.com>
2023-02-28 13:19:14 +0300

    update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // App Environment.
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
+
+        // App Db Schema.
+        Schema::defaultStringLength(191);
     }
 }
