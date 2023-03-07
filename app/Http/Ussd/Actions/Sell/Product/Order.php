b84621546bf5805a6ccb44b4ac64edb7a2823b10
Wagura Maurice <business@waguramaurice.com>
2023-03-08 00:33:38 +0300

    update Order.php

--- app/Http/Ussd/Actions/Sell/Product/Order.php
+++ app/Http/Ussd/Actions/Sell/Product/Order.php
@@ -8,6 +8,47 @@ class Order extends Action
 {
     public function run(): string
     {
-        return ''; // The state after this
+        try {
+            $curl = curl_init();
+
+            curl_setopt_array($curl, [
+                CURLOPT_URL => 'https://gizpassion.com/api/sell/',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => json_encode([
+                    'phone_number' => substr($this->record->get('phoneNumber'), -9),
+                    'grade' => $this->record->get('variety'),
+                    'metric' => (int) $this->record->get('quantity')
+                ]),
+                CURLOPT_HTTPHEADER => [
+                    'Content-Type: application/json'
+                ]
+            ]);
+
+            $error    = curl_error($curl);
+            $response = json_decode(curl_exec($curl));
+
+            curl_close($curl);
+
+            if ($error) {
+                $this->record->set('prompt', __('Oops! server error encountered, please try again!'));
+            } else {
+                if (optional($response)->order_id) {
+                    $this->record->set('prompt', __('Congratulations! Order ' . strtoupper($response->order_id) . ' was successfully created.'));
+                } else {
+                    $this->record->set('prompt', __('Oops! Order NOT successfully created, please try again!'));
+                }
+            }
+        } catch (\Throwable $th) {
+            // throw $th;
+            eThrowable(get_class($this), $th->getMessage());
+        }
+
+        return \App\Http\Ussd\States\Terminate::class; // The state after this
     }
 }
