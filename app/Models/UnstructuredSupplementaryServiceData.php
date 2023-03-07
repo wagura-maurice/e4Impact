11588f90f3f8385d66459cfa30d056d8d3d87f36
Wagura Maurice <business@waguramaurice.com>
2023-03-08 00:33:28 +0300

    update UnstructuredSupplementaryServiceData.php

--- app/Models/UnstructuredSupplementaryServiceData.php
+++ app/Models/UnstructuredSupplementaryServiceData.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Validation\Rule;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class UnstructuredSupplementaryServiceData extends Model
 {
