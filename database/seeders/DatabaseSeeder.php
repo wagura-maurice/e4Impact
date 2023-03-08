8171edfeb1b41f482f00ae0c036996172e82cb51
Wagura Maurice <business@waguramaurice.com>
2023-03-08 09:45:50 +0300

    create 1 file and update 1 file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call(SettingsTableSeeder::class);
         $this->call(CommunicationCategoriesTableSeeder::class);
+        $this->call(CountiesTableSeeder::class);
     }
 }
