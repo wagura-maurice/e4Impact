bd8509fd807621e462dad2665453c63cf33803de
Wagura Maurice <business@waguramaurice.com>
2023-03-14 11:16:50 +0300

    update Gender.php

--- app/Http/Ussd/States/Account/Create/Gender.php
+++ app/Http/Ussd/States/Account/Create/Gender.php
@@ -25,7 +25,7 @@ class Gender extends State
         }
 
         $this->decision
-            ->between(1, 2, \App\Http\Ussd\States\Account\Create\County::class)
+            ->between(1, 2, \App\Http\Ussd\States\Account\Create\Location::class)
             ->any(self::class);
     }
 }
