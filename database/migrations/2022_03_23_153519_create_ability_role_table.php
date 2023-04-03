8cc12a5f2f9fc0cf48c2d25388ad8b6edc3684d5
Wagura Maurice <business@waguramaurice.com>
2023-04-03 14:01:18 +0300

    update 1 file, create 4 files, copy 2 files and rename 1 file

---
+++ database/migrations/2022_03_23_153519_create_ability_role_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAbilityRoleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ability_role', function (Blueprint $table) {
+            // $table->id();
+            $table->primary(['role_id', 'ability_id']);
+            $table->foreignId('role_id')->unsigned()->constrained('roles')->onDelete('cascade');
+            $table->foreignId('ability_id')->unsigned()->constrained('abilities')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ability_role');
+    }
+}
---
+++ database/migrations/2022_03_23_153521_create_profiles_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string('_pid')->unique();
+            $table->foreignId('user_id')->unsigned()->unique()->constrained('users')->onDelete('cascade');
+            $table->string('telephone')->nullable()->unique();
+            $table->string('national_id')->nullable()->unique();
+            $table->string('first_name');
+            $table->string('middle_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->integer('gender')->nullable();
+            $table->string('address_line_1')->nullable();
+            $table->string('address_line_2')->nullable();
+            $table->string('city')->nullable();
+            $table->string('state')->nullable();
+            $table->string('country')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
