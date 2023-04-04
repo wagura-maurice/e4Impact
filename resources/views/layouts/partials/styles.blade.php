d34d1104ee10506f30ba2481e23fca06c66e2b17
Wagura Maurice <business@waguramaurice.com>
2023-04-05 00:49:34 +0300

    update scripts.blade.php and styles.blade.php

--- resources/views/layouts/partials/styles.blade.php
+++ resources/views/layouts/partials/styles.blade.php
@@ -1,9 +1,9 @@
+<!-- Favicons -->
+<link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}" type="image/x-icon">
 <!-- Styles -->
 <link rel="stylesheet" href="{{ asset('css/main/app.css') }}">
 <link rel="stylesheet" href="{{ asset('css/main/app-dark.css') }}">
-<!-- Favicons -->
-<link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}" type="image/x-icon">
-<!-- Custom Styles -->
-@stack('styles')
 <!-- Livewire Styles -->
 @livewireStyles
+<!-- Custom Styles -->
+@stack('styles')
